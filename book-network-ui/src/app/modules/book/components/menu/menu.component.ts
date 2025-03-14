1294ef8944c5a205b04c5224e57a9a3f5e0a5075
TariqKichawele <tariqkichawele66@gmail.com>
2025-03-14 16:58:08 -0400

    Add book module to BookNetwork UI with routing, main component, and menu component implementation

---
+++ book-network-ui/src/app/modules/book/components/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+  logout() {};
+}
